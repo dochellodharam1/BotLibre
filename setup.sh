c1bf4a54595e3bb64eea2e779fd2f3a10c61961b
Neetesh Bhardwaj <neebhard@publicisgroupe.net>
2020-03-28 14:53:24 +0530

    Removed extra items

--- setup.sh
+++ setup.sh
@@ -1,10 +1,1 @@
-
-
-echo "CACHE_DIR ::: $CACHE_DIR"
-
-MVN_ALIAS=`alias mvn`
-MVN_CMD=`echo "$MVN_ALIAS" | cut -d"'" -f2`
-
-sed -i "s@mvn@$MVN_CMD@g" lib/deploy_maven_libs.sh
-
 . lib/deploy_maven_libs.sh
